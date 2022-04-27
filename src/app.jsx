1cbe322612a2cfdcc573f5822db8a92b59aff4cb
HyeongJin Kim <khj7741@gmail.com>
2022-04-28 02:39:21 +0900

    get fetch and print videos title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,31 @@
-import './app.css';
+import "./app.css";
+import React, { useEffect, useState } from "react";
+import Videos from "./videos.jsx";
 
-function App() {
-  return <h1>hello~!</h1>
-}
+const App = (props) => {
+  const YOUTUBE_APIKEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const [videos, setVideos] = useState([]);
+  const getVideos = () => {
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&key=${YOUTUBE_APIKEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const videosInfo = data.items.map((item) => {
+          return item.snippet;
+        });
+        setVideos(videosInfo);
+      });
+  };
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  return (
+    <div>
+      <Videos videos={videos} />
+    </div>
+  );
+};
 
 export default App;
